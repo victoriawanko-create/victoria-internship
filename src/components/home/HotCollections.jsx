9e4985f19953996e1b7c9a971dcbdc4353736e2a
victoriawanko-create. <victoriawanko@gmail.com.>
2026-08-26 13:33:42 -0400

    finished loading state

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
 import Slider from "react-slick";
+
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./HotCollections.css";
@@ -20,25 +19,28 @@ const setting = {
 };
 
   useEffect (() => {
-    async function fetchData() {
+    let timer;
 
-   const response = await fetch(
+    async function fetchData() {
+      setLoading(true);
+
+      try {
+       const response = await fetch(
       "https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections"
     );
-    const result = await response.json();
-    setData(result);
-    
-    setTimeout(() => {
-    setLoading(false);
-    }, 2000);
+
+    const result = await response.json(); 
+        setData(result);
+      } catch (error) {
+        console.error("Failed to fetch data")
+      } finally {
+        timer = setTimeout(() => setLoading(false), 2000) 
+      };
   }
 
   fetchData();
+  return () => clearTimeout(timer);
 }, []);
-
-if (loading) {
-  return <div>Loading...</div>;
-}
 
   return (
     <section id="section-collections" className="no-bottom">
@@ -52,9 +54,12 @@ if (loading) {
           </div>
 
           <Slider {...setting}>
-          {data.map((item) => {
-
-            return (
+          {loading ? 
+          (new Array(4).fill(0)
+          .map((_, index) => <div key={index}>
+            <div className="nft_coll">
+              <div className="nft_wrap skeleton"></div></div></div>)) : (data.map((item) => (
+          
              <div key={item.nftId}>
               <div className="nft_coll">
                 <div className="nft_wrap">
@@ -76,8 +81,7 @@ if (loading) {
                 </div>
               </div>
             </div>
-            );
-          })}
+           )))}
           </Slider>
         </div>
       </div>
